fa30ebd6204e0e3ccdab04d24f5619b0f4aa9b1a
FArekkusu <arekusandaafedorofu@gmail.com>
2020-06-05 00:55:41 +0300

    Reworked entry and note testing

--- app/src/androidTest/java/com/example/nought/DatabaseTest.kt
+++ app/src/androidTest/java/com/example/nought/DatabaseTest.kt
@@ -75,15 +75,71 @@ class DatabaseTest {
 
     @Test
     @Throws(IOException::class)
-    fun entriesLifecycle() {
+    fun entryLifecycle() {
+        // No entries exist yet
+        dao.insert(Note(title = ""))
+        val note = dao.getAllNotesSync()[0].note
+        var allEntries = dao.getAllEntriesSync(note.id)
+        assertEquals(0, allEntries.size)
+
+
+        // 2 entries were added
+        val emptyString = ""
+        dao.insert(Entry(content = emptyString, noteId = note.id, markable = true, marked = false))
+        dao.insert(Entry(content = emptyString, noteId = note.id, markable = false, marked = false))
+        allEntries = dao.getAllEntriesSync(note.id)
+        assertEquals(2, allEntries.size)
+
+
+        // Second entry was removed, first remains
+        dao.deleteEntry(allEntries[1].id)
+        val remainingEntries = dao.getAllEntriesSync(note.id)
+        assertEquals(1, remainingEntries.size)
+        assertEquals(allEntries[0], remainingEntries[0])
+
+
+        // Fetching entry by id works correctly
+        var entry = dao.getEntrySync(allEntries[0].id)
+        assertEquals(entry, allEntries[0])
+
+
+        // Entry was marked
+        dao.mark(entry.id)
+        assertEquals(true, dao.getEntrySync(entry.id).marked)
+
+
+        // Content was updated, and entry was unmarked
+        val newText = "Not empty string"
+        dao.updateText(entry.id, newText)
+        entry = dao.getEntrySync(entry.id)
+        assertEquals(newText, entry.content)
+        assertEquals(false, entry.marked)
+
+
+        // Repeated marking switches between marked and unmarked state
+        dao.mark(entry.id)
+        assertEquals(true, dao.getEntrySync(entry.id).marked)
+        dao.mark(entry.id)
+        assertEquals(false, dao.getEntrySync(entry.id).marked)
+
+
+        // Deleting a note deletes its entries too
+        dao.deleteNote(note.id)
+        assertEquals(0, dao.getAllNotesSync().size)
+        assertEquals(0, dao.getAllEntriesForAllNotesSync().size)
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun notesAndEntriesLifecycle() {
         // No notes exist
         var allNotes = dao.getAllNotesSync()
         assertEquals(0, allNotes.size)
 
 
         // Correct initial totals
-        dao.insert(Note(title = "Note 1"))
-        dao.insert(Note(title = "Note 2"))
+        dao.insert(Note(title = ""))
+        dao.insert(Note(title = ""))
         allNotes = dao.getAllNotesSync()
         assertEquals(2, allNotes.size)
         assertEquals(0, allNotes[0].markedEntries)
@@ -93,35 +149,18 @@ class DatabaseTest {
 
 
         // Insert entries for note #1
-        val sampleText = ""
-        dao.insert(Entry(content = sampleText, noteId = allNotes[0].note.id, markable = true, marked = false))
-        dao.insert(Entry(content = sampleText, noteId = allNotes[0].note.id, markable = true, marked = true))
+        val emptyString = ""
+        dao.insert(Entry(content = emptyString, noteId = allNotes[0].note.id, markable = true, marked = true))
 
         // Insert entries for note #2
-        dao.insert(Entry(content = sampleText, noteId = allNotes[1].note.id, markable = false, marked = false))
-        dao.insert(Entry(content = sampleText, noteId = allNotes[1].note.id, markable = true, marked = false))
-        dao.insert(Entry(content = sampleText, noteId = allNotes[1].note.id, markable = true, marked = true))
+        dao.insert(Entry(content = emptyString, noteId = allNotes[1].note.id, markable = false, marked = false))
+        dao.insert(Entry(content = emptyString, noteId = allNotes[1].note.id, markable = true, marked = false))
+        dao.insert(Entry(content = emptyString, noteId = allNotes[1].note.id, markable = true, marked = true))
 
 
-        // Note #1 has 2 entries
+        // Note #1 has 1 entry
         var allEntries = dao.getAllEntriesSync(allNotes[0].note.id)
-        assertEquals(2, allEntries.size)
-
-
-        // second entry was deleted from note #1, and the first one remains
-        dao.deleteEntry(allEntries[1].id)
-        val remainingEntries = dao.getAllEntriesSync(allNotes[0].note.id)
-        assertEquals(1, remainingEntries.size)
-        assertEquals(allEntries[0].id, remainingEntries[0].id)
-
-
-        // Both notes exist, and their marked/total counts are up to date
-        allNotes = dao.getAllNotesSync()
-        assertEquals(2, allNotes.size)
-        assertEquals(0, allNotes[0].markedEntries)
-        assertEquals(1, allNotes[0].totalEntries)
-        assertEquals(1, allNotes[1].markedEntries)
-        assertEquals(2, allNotes[1].totalEntries)
+        assertEquals(1, allEntries.size)
 
 
         // Note #2 still has 3 entries
@@ -129,50 +168,45 @@ class DatabaseTest {
         assertEquals(3, allEntries.size)
 
 
-        // Entry count didn't change, second entry has updated text
-        var newText = "I was empty before"
+        // Both notes exist, and their marked/total counts are up to date
+        allNotes = dao.getAllNotesSync()
+        assertEquals(2, allNotes.size)
+        assertEquals(1, allNotes[0].markedEntries)
+        assertEquals(1, allNotes[0].totalEntries)
+        assertEquals(1, allNotes[1].markedEntries)
+        assertEquals(2, allNotes[1].totalEntries)
+
+
+        // Entry count didn't change, only second entry of second note has updated text
+        val newText = "I was empty before"
         dao.updateText(allEntries[1].id, newText)
+
+        allEntries = dao.getAllEntriesSync(allNotes[0].note.id)
+        assertEquals(1, allEntries.size)
+        assertEquals(emptyString, allEntries[0].content)
+
         allEntries = dao.getAllEntriesSync(allNotes[1].note.id)
         assertEquals(3, allEntries.size)
-        assertEquals(sampleText, allEntries[0].content)
+        assertEquals(emptyString, allEntries[0].content)
         assertEquals(newText, allEntries[1].content)
-        assertEquals(sampleText, allEntries[2].content)
+        assertEquals(emptyString, allEntries[2].content)
 
 
         // Second note's marked count is up to date, first note's marked count didn't change
         dao.mark(allEntries[1].id)
         allNotes = dao.getAllNotesSync()
         assertEquals(2, allNotes.size)
-        assertEquals(0, allNotes[0].markedEntries)
+        assertEquals(1, allNotes[0].markedEntries)
         assertEquals(1, allNotes[0].totalEntries)
         assertEquals(2, allNotes[1].markedEntries)
         assertEquals(2, allNotes[1].totalEntries)
-
-
-        // Updating marked entry unmarked it
-        newText = "I was not empty just now"
-        dao.updateText(allEntries[1].id, newText)
-        assertEquals(false, dao.getAllEntriesSync(allNotes[1].note.id)[1].marked)
-
-
-        // Marking an entry twice marks and unmarks it
-        dao.mark(allEntries[1].id)
-        assertEquals(true, dao.getAllEntriesSync(allNotes[1].note.id)[1].marked)
-        dao.mark(allEntries[1].id)
-        assertEquals(false, dao.getAllEntriesSync(allNotes[1].note.id)[1].marked)
-
-
-        // Entry is fetched correctly by id
-        val entry22 = dao.getEntrySync(allEntries[1].id)
-        assertEquals(entry22.id, allEntries[1].id)
-        assertEquals(entry22.content, newText)
 
 
         // All entries of note #2 unmarked, note #1 is untouched
         dao.unmarkAll(allNotes[1].note.id)
         allNotes = dao.getAllNotesSync()
         assertEquals(2, allNotes.size)
-        assertEquals(0, allNotes[0].markedEntries)
+        assertEquals(1, allNotes[0].markedEntries)
         assertEquals(1, allNotes[0].totalEntries)
         assertEquals(0, allNotes[1].markedEntries)
         assertEquals(2, allNotes[1].totalEntries)
